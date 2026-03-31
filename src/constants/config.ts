2fdeeb5abbbda9b10a18c6ba59fdd5c92e2f5160
Alina Potapchuk <alinapotapchuk7@gmail.com>
2026-03-31 14:16:09 +0300

    Fix constant of server

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,6 @@
 // ⚠️ IP ВАШ!
-export const SERVER_URL = 'http://192.168.1.100:3000';
+//export const SERVER_URL = 'http://192.168.1.100:3000';
+export const SERVER_URL = 'https://alinagramm-server-production.up.railway.app';
 
 export const STORAGE_KEYS = {
   MESSAGES: (userId: string, contact: string) => `messages_${userId}_${contact}`,
